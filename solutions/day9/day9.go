c5965cfc9e7ea42cfd7c2c981412710d2f3b797d
Riley Cohn <cohriley@amazon.com>
2021-12-08 23:31:46 -0700

    Day 9 pt 2

--- solutions/day9/day9.go
+++ solutions/day9/day9.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventOfCode2021/lib"
 	"fmt"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -26,8 +27,7 @@ func main() {
 	}
 
 	// Now we have the grid, find the low points
-	// lowPoints := [][]int{}
-	largestBasins := [3]int{}
+	lowPoints := []string{}
 	for i, h := range grid {
 		for j, cell := range h {
 			left :=  j - 1
@@ -39,15 +39,87 @@ func main() {
 				!(right < len(h) && cell >= grid[i][right]) &&
 				!(up >= 0 && cell >= grid[up][j]) &&
 				!(down < len(grid) && cell >= grid[down][j]) {
-				fmt.Println("Found low point")
-				fmt.Println(i)
-				fmt.Println(j)
-				fmt.Println(grid[i][j])
+				lowPoints = append(lowPoints, strconv.Itoa(j) + "," + strconv.Itoa(i))
 				answer += 1 + grid[i][j]
 			}
 		}
-		fmt.Println()
 	}
 
-	fmt.Println(answer)
+	// We have all the low points, find basins
+	largestBasins := []int{}
+	for _, lowPoint := range lowPoints {
+		visited := [100][100]bool{}
+		visited = getNeighboringNonNines(grid, lowPoint, visited)
+
+		// Check how many trues are in visited
+		trues := 0
+		for i, h := range visited {
+			for j, _ := range h {
+				if visited[i][j] {
+					trues++
+				}
+			}
+		}
+
+		largestBasins = append(largestBasins, trues)
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(largestBasins)))
+
+	result := 1
+	for num := 0; num < 3; num++ {
+		result *= largestBasins[num]
+	}
+
+	fmt.Println(result)
+}
+
+func getNeighboringNonNines(grid [100][100]int, point string, visited [100][100]bool) [100][100]bool {
+	xy := strings.Split(point, ",")
+	x,_ := strconv.Atoi(xy[0])
+	y,_ := strconv.Atoi(xy[1])
+
+	visited[y][x] = true
+
+	// Go left
+	for i := x - 1; i >= 0; i-- {
+		if grid[y][i] != 9 && !visited[y][i] {
+			visited[y][i] = true
+			visited = getNeighboringNonNines(grid, strconv.Itoa(i) +"," +strconv.Itoa(y), visited)
+		} else {
+			break
+		}
+	}
+
+	// Go right
+	for i := x + 1; i < len(grid[0]); i++ {
+		if grid[y][i] != 9 && !visited[y][i] {
+			visited[y][i] = true
+			visited = getNeighboringNonNines(grid, strconv.Itoa(i) +"," +strconv.Itoa(y), visited)
+		} else {
+			break
+		}
+	}
+
+	// Go up
+	for i := y - 1; i >= 0; i-- {
+		if grid[i][x] != 9 && !visited[i][x] {
+			visited[i][x] = true
+			visited = getNeighboringNonNines(grid, strconv.Itoa(x) +"," +strconv.Itoa(i), visited)
+		} else {
+			break
+		}
+	}
+
+	// Go down
+	for i := y + 1; i < len(grid); i++ {
+		if grid[i][x] != 9 && !visited[i][x] {
+			visited[i][x] = true
+			visited = getNeighboringNonNines(grid, strconv.Itoa(x) +"," +strconv.Itoa(i), visited)
+		} else {
+			break
+		}
+	}
+
+	return visited
 }
